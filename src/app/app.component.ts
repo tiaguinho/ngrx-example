57db5a7544c23c92e3877011718bda7b92b3a4a4
Tiago Temporin <tiaguinhocab@gmail.com>
2017-11-20 18:25:19 +0000

    add method and reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,41 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup
+} from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-import { Todo } from 'app/models/todo';
-import * as fromTodos from 'app/reducers/todos';
+import { Todo } from 'app/todo/models/todo';
+import * as todo from 'app/todo/actions/todo';
+import * as fromTodos from 'app/todo/reducers/todos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
 
-  todos: Observable<Todo>;
+  todos: Observable<Todo[]>;
 
-  constructor(store: Store<fromTodos.State>) {
+  todoForm = new FormGroup({
+    task: new FormControl()
+  });
 
+  constructor(private store: Store<fromTodos.State>) {
+    this.todos = store.select(fromTodos.getTodosAll);
+  }
+
+  add(): void {
+    const data: Todo = this.todoForm.value;
+    data.id = new Date().getMilliseconds();
+    data.done = false;
+
+    this.store.dispatch(new todo.Add(data));
   }
 
 }
