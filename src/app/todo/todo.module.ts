04d2de31c5fe5c01874ba255f236687babd4909a
Tiago Temporin <tiaguinhocab@gmail.com>
2017-11-23 06:16:04 +0000

    add effects

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { TodoComponent } from './components/todo/todo.component';
 import { TodosComponent } from './components/todos/todos.component';
 
+import { TodoEffects } from 'app/todo/effects/todos';
 import * as todos from 'app/todo/reducers/todos';
 
 @NgModule({
   imports: [
     CommonModule,
-    StoreModule.forFeature('todos', todos.reducer)
+    StoreModule.forFeature('todos', todos.reducer),
+    EffectsModule.forFeature([TodoEffects])
   ],
   declarations: [
     TodoComponent,
