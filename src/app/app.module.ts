4f7d62abf99a337ba28e00fa0714cbb717a60549
Tiago Temporin <tiaguinhocab@gmail.com>
2017-11-23 06:14:10 +0000

    add effects and store devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { TodoModule } from 'app/todo/todo.module';
 
 import { AppComponent } from './app.component';
@@ -14,7 +16,11 @@ import { AppComponent } from './app.component';
     BrowserModule,
     ReactiveFormsModule,
     TodoModule,
-    StoreModule.forRoot([])
+    StoreModule.forRoot([]),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
