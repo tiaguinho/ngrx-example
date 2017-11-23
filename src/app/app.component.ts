c934e2b371c6fd6b282ded6856c5857704f28899
Tiago Temporin <tiaguinhocab@gmail.com>
2017-11-23 06:35:32 +0000

    improve code and add comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import {
   Component,
-  ChangeDetectionStrategy
+  ChangeDetectionStrategy,
+  OnInit
 } from '@angular/core';
 import {
-  FormControl,
-  FormGroup
+  FormBuilder,
+  FormGroup,
+  Validators
 } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { Todo } from 'app/todo/models/todo';
+
 import * as todo from 'app/todo/actions/todo';
 import * as fromTodos from 'app/todo/reducers/todos';
 
@@ -18,24 +21,51 @@ import * as fromTodos from 'app/todo/reducers/todos';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  /**
+   * Observable list of todo
+   */
   todos: Observable<Todo[]>;
 
-  todoForm = new FormGroup({
-    task: new FormControl()
-  });
+  /**
+   * Reactive form
+   */
+  form: FormGroup;
 
-  constructor(private store: Store<fromTodos.State>) {
+  constructor(
+    private store: Store<fromTodos.State>,
+    private formBuilder: FormBuilder
+  ) {
     this.todos = store.select(fromTodos.getTodosAll);
   }
 
-  add(): void {
-    const data: Todo = this.todoForm.value;
-    data.id = new Date().getMilliseconds();
-    data.done = false;
+  ngOnInit() {
+    // crate reactive form
+    this.form = this.formBuilder.group({
+      id: [''],
+      task: ['', Validators.required],
+      complete: ['']
+    });
 
-    this.store.dispatch(new todo.Add(data));
+    // subscribe to receive selected todo
+    this.store.select(fromTodos.getSelectedTodo).subscribe(todo => {
+      if (!todo) return;
+
+      this.form.setValue(todo);
+    });
+  }
+
+  /**
+   * Submit the form
+   * @param { value, valid }: { value: Todo, valid: boolean }
+   */
+  onSubmit({value, valid}: {value: Todo, valid: boolean}): void {
+    if (valid) {
+      // dispatch new action
+      this.store.dispatch(new todo.Save(value));
+      this.form.reset();
+    }
   }
 
 }
